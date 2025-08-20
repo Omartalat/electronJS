89d170aef1965b2c85d65cead4da67775dbd3f1a
omar-talat <dr.omartalat@gmail.com>
2025-08-21 02:02:13 +0300

    Initialize Electron main process: set up app and main window creation for Image Resize application

--- image_resize/main.js
+++ image_resize/main.js
@@ -1,1 +1,13 @@
-console.log("Hello, World!");
+const { app, BrowserWindow } = require("electron/main");
+
+function createWindow(BrowserWindow) {
+  const mainWindow = new BrowserWindow({
+    title: "Image Resize",
+    width: 500,
+    height: 600,
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow(BrowserWindow);
+});
