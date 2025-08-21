e6cac5c9d9913d25170f68e0a3072440f82877bf
omar-talat <dr.omartalat@gmail.com>
2025-08-21 11:49:41 +0300

    Add HTML structure and icons for Image Resize application

--- image_resize/main.js
+++ image_resize/main.js
@@ -1,11 +1,16 @@
 const { app, BrowserWindow } = require("electron/main");
 
+let mainWindow;
+
 function createWindow(BrowserWindow) {
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     title: "Image Resize",
     width: 500,
     height: 600,
+    icon: "./assets/icons/Icon_256x256.png",
   });
+
+  mainWindow.loadFile("./app/index.html");
 }
 
 app.whenReady().then(() => {
