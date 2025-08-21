e999b31dbcc2674de244892eb282944c2a722283
omar-talat <dr.omartalat@gmail.com>
2025-08-21 12:57:01 +0300

    Refactor main.js: Correct Electron import, update icon paths, and simplify createWindow function

--- image_resize/main.js
+++ image_resize/main.js
@@ -1,18 +1,25 @@
-const { app, BrowserWindow } = require("electron/main");
+const { app, BrowserWindow } = require("electron");
+
+process.env.NODE_ENV = "development";
+
+const isDev = process.env.NODE_ENV !== "production";
+
+const isWin = process.platform === "win32";
 
 let mainWindow;
 
-function createWindow(BrowserWindow) {
+function createWindow() {
   mainWindow = new BrowserWindow({
     title: "Image Resize",
     width: 500,
     height: 600,
-    icon: "./assets/icons/Icon_256x256.png",
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: isDev,
   });
 
-  mainWindow.loadFile("./app/index.html");
+  mainWindow.loadFile(`${__dirname}/app/index.html`);
 }
 
 app.whenReady().then(() => {
-  createWindow(BrowserWindow);
+  createWindow();
 });
